3a754aad724c5042c4804949c561e64e1ab9ad0c
Fabiano França <ffranca@ciandt.com>
2016-01-07 22:55:58 -0200

    Let the game begin

---
+++ samples/src/main/java/com/appolo_lib/textplug_samples/MainActivity.java
@@ -0,0 +1,13 @@
+package com.appolo_lib.textplug_samples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
