6d8154627b68d8b272aaaeace0247b28c98b7c3f
Fabiano França <ffranca@ciandt.com>
2016-02-07 22:53:13 -0200

    Setup test class

---
+++ textplug-sample/src/androidTest/java/com/appolo_lib/textplug_samples/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.appolo_lib.textplug_samples;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void test(){
+    }
+}
